2894cc07f3b3dd3c608962bbf7a0e959d0059715
Derell Licht <derelict@comcast.net>
2025-06-18 17:06:46 -0700

    merge reference lint exclusions file

--- lintdefs.cpp
+++ lintdefs.cpp
@@ -1,11 +1,2 @@
-//  
-
-//lint -e10    Expecting ';'
 //lint -e534   Ignoring return value of function
-//lint -e1066  Symbol 'conio_min::hStdOutPublic' declared as "C" conflicts ...
-//lint -e1776  Converting a string literal is not const safe (initialization)
 
-//lint -esym(14,  __created)   Symbol not referenced
-//lint -esym(401, __created)   Symbol not referenced
-//lint -esym(528, __created)   Symbol not referenced
-//lint -esym(843, __created)   Variable could be declared as const
